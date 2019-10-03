f6e45d84d26a4dc8c95d9a325b672bd09f720397
Melvin Carvalho <melvincarvalho@gmail.com>
2019-10-03 17:39:17 +0200

    DEFAULT_MESSAGE

--- index.mm
+++ index.mm
@@ -64,7 +64,7 @@
 </stylenode>
 </map_styles>
 </hook>
-<hook NAME="AutomaticEdgeColor" COUNTER="4" RULE="ON_BRANCH_CREATION"/>
+<hook NAME="AutomaticEdgeColor" COUNTER="5" RULE="ON_BRANCH_CREATION"/>
 <node TEXT="wallets" POSITION="right" ID="ID_403237537" CREATED="1569861472802" MODIFIED="1569861865685">
 <icon BUILTIN="password"/>
 <edge COLOR="#007c00"/>
@@ -267,6 +267,9 @@
 </html>
 </richcontent>
 </node>
+</node>
+<node TEXT="fork" POSITION="right" ID="ID_1679611241" CREATED="1570117107113" MODIFIED="1570117108545">
+<edge COLOR="#00ffff"/>
 </node>
 <node TEXT="links" POSITION="left" ID="ID_728396892" CREATED="1569861329090" MODIFIED="1569861329093">
 <icon BUILTIN="attach"/>
